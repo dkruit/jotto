43d810b4dc97acec758e3670b62fceecfe3b93d1
DirkKruit | Cergentis <dirk.kruit@cergentis.com>
2022-11-05 21:35:51 +0100

    Working but slow

--- find_solutions.rs
+++ find_solutions.rs
@@ -1,49 +1,41 @@
 use std::collections::HashMap;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::io::Error;
+use std::path::Path;
+use std::fs::File;
 
 static ALPHABET: &str = "qxjzvfwbkgpmhdcytlnuroisea";
+static INPUTFILE: &str = "words_alpha.txt";
+static OUTPUTFILE: &str = "rust_solutions.csv";
 
-fn main() {
-    let word1 = "fjord";
-    let word2 = "vibex";
-    let word3 = "freax";
+fn main() -> Result<(), Error> {
+    let filepath = Path::new(INPUTFILE);
+    let mut words_map: HashMap<char, Vec<String>> = HashMap::new();
 
-    let old_words: &Vec<&str> = &vec![word1];
+    // Read file
+    let file = match File::open(&filepath) {
+        Err(why) => panic!("couldn't open file '{}': {}", filepath.display(), why),
+        Ok(file) => file,
+    };
 
-    let is_valid = word_has_unique_letters(&word1);
-    println!("{} validity: {}", word1, is_valid);
+    let reader = BufReader::new(file);
 
-    let uniqe_letters = words_have_unique_letters(&old_words, &word3);
-    println!("{} and {} do not share letters: {}", old_words[0], word3, uniqe_letters);
+    for line in reader.lines() {
+        let word = line?;
+        if word.len() == 5 && word_has_unique_letters(&word) {
+            let letter = first_letter_in_word(&word);
+            words_map.entry(letter).or_insert(Vec::new()).push(word.clone());
 
-    let uniqe_letters = words_have_unique_letters(&old_words, &word2);
-    println!("{} and {} do not share letters: {}", old_words[0], word2, uniqe_letters);
-
-    let first_letter = first_letter_in_word(&word3);
-    println!("First letter of {} is {}", word3, first_letter);
-
-    let first_letter = first_letter_in_word(&word2);
-    println!("First letter of {} is {}", word2, first_letter);
-
-    let removed_fjord = remove_letters_in_solution(&old_words);
-    println!("Alphabet without {}: {}", old_words[0], removed_fjord);
-
-    let removed_fjord = remove_letters_in_solution(&vec![word2]);
-    println!("Alphabet without {}: {}", word2, removed_fjord);
-
-    let removed_fjord = remove_letters_in_solution(&vec![word2, word3]);
-    println!("Alphabet without {} and {}: {}", word2, word3, removed_fjord);
-
-    let words_map = read_input_to_hashmap(&vec![word1, word2, word3]);
-    for (letter, word_list) in words_map {
-        println!("Words with {}:", letter);
-        for word in word_list{
-            println!("{}", word);
         }
     }
-
+    let solutions = find_solutions(&words_map);
+    let n_solutions = solutions.len();
+    println!("Found {} solutions", n_solutions);
+    return Ok(())
 }
 
-fn word_has_unique_letters(word: &str) -> bool {
+fn word_has_unique_letters(word: &String) -> bool {
     let mut result: bool = true;
     for (i, letter_1) in word.get(..word.len()-1).expect("REASON").chars().enumerate(){
         for letter_2 in word.get(i+1..).expect("REASON").chars(){
@@ -55,7 +47,7 @@ fn word_has_unique_letters(word: &str) -> bool {
     return result;
 }
 
-fn words_have_unique_letters(old_words: &Vec<&str>, new_word: &str) -> bool {
+fn words_have_unique_letters(old_words: &Vec<String>, new_word: &String) -> bool {
     let mut result = true;
     for old_word in old_words.iter(){
         for letter_1 in old_word.chars(){
@@ -80,7 +72,7 @@ fn first_letter_in_word(word: &str) -> char {
     return '_'
 }
 
-fn remove_letters_in_solution(solution: &Vec<&str>) -> String {
+fn remove_letters_in_solution(solution: &Vec<String>) -> String {
     let mut letters = String::from(ALPHABET);
     for word in solution.iter(){
         for letter in word.chars(){
@@ -90,13 +82,81 @@ fn remove_letters_in_solution(solution: &Vec<&str>) -> String {
     return letters;
 }
 
-fn read_input_to_hashmap(words: &Vec<&str>) -> HashMap<char, Vec<String>> {
-    let mut words_map: HashMap<char, Vec<String>> = HashMap::new();
+fn find_solutions(words_map: &HashMap<char, Vec<String>>) -> Vec<Vec<String>> {
+    let mut solutions: Vec<Vec<String>> = Vec::new();
 
-    for word in words.iter(){
-        let letter = first_letter_in_word(&word);
-        words_map.entry(letter).or_insert(Vec::new()).push(word.to_string());
+    for (i_1, letter_1) in ALPHABET.get(..2).expect("REASON").chars().enumerate() {
+        println!("Finding solutions with a word containing the letter {} (iteration {}/2)",
+                 letter_1, i_1+1);
+
+        for word_1 in &words_map[&letter_1] {
+            let mut solution = vec!(word_1.clone());
+            let mut remaining_letters = remove_letters_in_solution(&solution);
+            println!("Finding solutions with {}", word_1);
+
+            for (i_2, letter_2) in remaining_letters.clone().get(..2).expect("Reason").chars()
+                .enumerate() {
+                if i_1 == 1 && i_2 == 0 { continue; }
+
+                for word_2 in &words_map[&letter_2] {
+                    solution = vec!(word_1.clone());
+                    if words_have_unique_letters(&solution, word_2) {
+                        solution.push(word_2.clone());
+                        remaining_letters = remove_letters_in_solution(&solution);
+                    }
+
+                    for (i_3, letter_3) in remaining_letters.clone().get(..2).expect("Reason")
+                        .chars().enumerate() {
+                        if solution.len() < 2 { break }
+                        if i_2 == 1 && i_3 == 0 { continue }
+
+                        for word_3 in &words_map[&letter_3] {
+                            solution = vec!(word_1.clone(), word_2.clone());
+                            if words_have_unique_letters(&solution, word_3) {
+                                solution.push(word_3.clone());
+                                remaining_letters = remove_letters_in_solution(&solution);
+                            }
+
+                            for (i_4, letter_4) in remaining_letters.clone()
+                                .get(..2).expect("Reason")
+                                .chars().enumerate() {
+                                if solution.len() < 3 { break }
+                                if i_3 == 1 && i_4 == 0 { continue }
+
+                                for word_4 in &words_map[&letter_4] {
+                                    solution = vec!(word_1.clone(), word_2.clone(), word_3.clone());
+                                    if words_have_unique_letters(&solution,
+                                                                 word_4) {
+                                        solution.push(word_4.clone());
+                                        remaining_letters = remove_letters_in_solution(&solution);
+                                    }
+
+                                    for (i_5, letter_5) in remaining_letters.clone()
+                                        .get(..2).expect("Reason")
+                                        .chars().enumerate() {
+                                        if solution.len() < 4 { break }
+                                        if i_4 == 1 && i_5 == 0 { continue }
+
+                                        for word_5 in &words_map[&letter_5] {
+                                            solution = vec!(word_1.clone(),
+                                                            word_2.clone(),
+                                                            word_3.clone(),
+                                                            word_4.clone());
+                                            if words_have_unique_letters(&solution,
+                                                                         word_5) {
+                                                solution.push(word_5.clone());
+                                                solutions.push(solution.clone())
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
-
-    return words_map;
+    return solutions
 }
